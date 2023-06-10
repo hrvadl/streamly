d6a6bfe7f81bdde92532674832bfc285ba12b771
hrvadl <youngwwad@gmail.com>
2023-06-10 21:09:08 +0300

    auth: added file structure

---
+++ auth/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/hrvadl/studdy-buddy/auth/pkg/config"
+	"github.com/hrvadl/studdy-buddy/auth/pkg/pb"
+	"github.com/hrvadl/studdy-buddy/auth/pkg/service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c := config.Load()
+	TCPServer, err := net.Listen("tcp", fmt.Sprintf(":%v", c.Port))
+
+	if err != nil {
+		log.Fatalf("cannot listen on TCP PORT %v %v", c.Port, err)
+	}
+
+	authService := service.New()
+
+	gRPCServer := grpc.NewServer()
+	pb.RegisterAuthServer(gRPCServer, authService)
+	gRPCServer.Serve(TCPServer)
+}
