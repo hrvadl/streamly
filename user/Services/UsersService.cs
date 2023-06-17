73363fcfe2b4c078e914a6b780a8115677f28812
Oleksandr Cherevkov <oleksandrcherevkov@gmail.com>
2023-06-17 14:44:31 +0300

    usr: update user service

--- user/Services/UsersService.cs
+++ user/Services/UsersService.cs
@@ -1,5 +1,7 @@
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using User;
+using User.Entities;
 using User.Infrastructure.Persistence;
 
 namespace User.Services;
@@ -7,28 +9,61 @@ namespace User.Services;
 public class UsersService : Users.UsersBase
 {
     private readonly ILogger<UsersService> logger;
-    private readonly UserDbContext context;
+    private readonly UserDbContext dbContext;
 
-    public UsersService(ILogger<UsersService> logger, UserDbContext context)
+    public UsersService(ILogger<UsersService> logger, UserDbContext dbContext)
     {
         this.logger = logger;
-        this.context = context;
+        this.dbContext = dbContext;
     }
 
-    public override Task<GetByIdResponse> GetById(GetByIdRequest request, ServerCallContext context)
+    public override async Task<CreateResponse> Create(CreateRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new GetByIdResponse
+        var user = new UserEntity
         {
-            Id = "guid",
-            Name = "Name",
-            Email = "Email.com",
-            Number = "+380500",
-            Description = null,
-            Role = new UserRole
+            Login = request.Login,
+            Email = request.Email,
+            Password = request.Password,
+        };
+
+        try
+        {
+            dbContext.Add(user);
+            await dbContext.SaveChangesAsync(context.CancellationToken);
+        }
+        catch (DbUpdateException ex)
+        {
+            var duplication = await dbContext.Users.Where(u => u.Login == request.Login || u.Email == request.Email).AnyAsync(context.CancellationToken);
+            if (duplication)
             {
-                Id = 1,
-                Name = "Student",
-            },
-        });
+                throw new RpcException(new Status(StatusCode.InvalidArgument, $"User with such email or login already exists."));
+            }
+            throw new RpcException(new Status(StatusCode.Internal, $"Exception on user saving occurred.", ex));
+        }
+        catch (Exception ex)
+        {
+            throw new RpcException(new Status(StatusCode.Internal, $"Exception on user saving occurred.", ex));
+        }
+
+        return new CreateResponse
+        {
+            Id = user.Id,
+        };
+    }
+
+    public override async Task<GetByIdResponse> GetById(GetByIdRequest request, ServerCallContext context)
+    {
+        var user = await dbContext.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == request.Id, context.CancellationToken);
+        if (user == null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, $"User with id {request.Id} does not exist."));
+        }
+
+        return new GetByIdResponse
+        {
+            Id = user.Id,
+            Login = user.Login,
+            Email = user.Email,
+        };
     }
 }
